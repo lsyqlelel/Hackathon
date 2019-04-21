5514691f050f0b0d9fd3071bae17f985591f3d7d
lsyqlelel <45878619+lsyqlelel@users.noreply.github.com>
2019-04-21 10:13:15 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,67 +1,81 @@
 <?php
-//header("content-type:text/html; charset=utf-8");
+header("content-type:text/html; charset=utf-8");
 $conn = new mysqli('localhost','root','root','hackathon');
-$monitor0=$_POST['monitor'];
-$message['id']=$_POST['id'];
-$message['password']=$_POST['password'];
+
+$monitor0 = $_POST['monitor'];
+$message['username'] = $_POST['username'];
+$message['password'] = $_POST['password'];
+$response=array();
 
 //账号格式是否正确
-if(ctype_digit($message['id']))
+if(ctype_digit($message['username']))//$message['username']为纯数字（必须是字符串类型），合法
 {
-            $x = 1;//账号是否存在
-            $sql0 = sprintf("select count(*) from student where id = '".$message['id']."'");
-            $result0 = mysqli_query($conn,$sql0);
-            $num = mysqli_fetch_array($result0,MYSQLI_NUM);
-            if($num[0]==0)
+    if($monitor0 == "0")//学生
+    {
+        $x1 = 1;
+        $sql0 = sprintf("select count(*) from t_students where id = '".$message['username']."'");
+        $result0 = mysqli_query($conn,$sql0);
+        $num = mysqli_fetch_array($result0,MYSQLI_NUM);
+        if($num[0] == 0)//学生账号不存在
+        {
+            $response["success"] = 0;//0账号不存在1密码错误2学生登录成功3班长登录成功4账号格式错误5无班长权限
+            $x1 = 0;
+        }
+        if($x1)//学生账号存在
+        {
+            $y1 = 1;
+            $sql1 = sprintf("select password from t_students where id = '".$message['username']."'");
+            $result1 = mysqli_query($conn,$sql1);
+            $s = $message['password'];
+            $pass = mysqli_fetch_array($result1,MYSQLI_NUM);
+            if($pass[0] != $s)//密码错误
             {
-                $loginjudge = 0;//0账号不存在1密码错误2学生登录成功3班长登录成功4账号格式错误
-                echo json_encode($loginjudge);
-                $x = 0;
+                $response["success"] = 1;
+                $y1 = 0;
             }
-
-            if($x==1 && $monitor0==0)//账号存在,学生
+            if($y1)//密码正确，学生登录成功
             {
-                $y = 1;
-                $sql1 = sprintf("select password from student where id = '".$message['id']."'");//比对密码是否正确
-                $result1 = mysqli_query($conn,$sql1);
-                $s = md5($message['password']);
-                $pass = mysqli_fetch_array($result1,MYSQLI_NUM);
-                if($pass[0]!=$s)
-                {
-                    $loginjudge = 1;
-                    echo json_encode($loginjudge);
-                    $y = 0;
-                }
-                if($y)//密码正确
-                {
-                    $loginjudge = 2;
-                    echo json_encode($loginjudge);
-                }
+                $response["success"] = 2;
             }
-            if($x==1 && $monitor==1)//账号存在，班长
+        }
+    }
+    if($monitor0 == "1")//班长
+    {
+        $x1 = 1;
+        $sql0 = sprintf("select count(*) from monitor where id = '".$message['username']."'");
+        $result0 = mysqli_query($conn,$sql0);
+        $num = mysqli_fetch_array($result0,MYSQLI_NUM);
+        if($num[0] == 0)//班长账号不存在
+        {
+            $response["success"] = 5;
+            $x1 = 0;
+        }
+        if($x1)//班长账号存在
+        {
+            $y1 = 1;
+            $sql1 = sprintf("select password from monitor where id = '".$message['username']."'");
+            $result1 = mysqli_query($conn,$sql1);
+            $s = $message['password'];
+            $pass = mysqli_fetch_array($result1,MYSQLI_NUM);
+            if($pass[0] != $s)//密码错误
             {
-                $y = 1;
-                $sql1 = sprintf("select password from monitor where id = '".$message['id']."'");//比对密码是否正确
-                $result1 = mysqli_query($conn,$sql1);
-                $s = md5($message['password1']);
-                $pass = mysqli_fetch_array($result1,MYSQLI_NUM);
-                if($pass[0]!=$s)
-                {
-                    $loginjudge = 1;
-                    echo json_encode($loginjudge);
-                    $y = 0;
-                }
-                if($y)//密码正确
-                {
-                    $loginjudge = 3;
-                    echo json_encode($loginjudge);
-                }
+                $response["success"] = 1;
+                $y1 = 0;
             }
+            if($y1)//密码正确，班长登录成功
+            {
+                $response["success"] = 3;
+            }
+        }
+    }
 }
 else
 {
-            $loginjudge = 4;//账号格式错误
-            echo json_encode($loginjudge);
+    $response["success"]= 4;//账号格式错误
 }
+
+echo json_encode($response);
+
+ini_set("error_reporting","E_ALL & ~E_NOTICE");
 mysqli_close($conn);
 ?>
